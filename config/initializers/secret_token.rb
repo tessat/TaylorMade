596e8c11c3ca5998b7d4dbdfb96c9339801d1529
Tessa Taylor <tessa.a.taylor@gmail.com>
2012-08-01 15:44:55 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaylorMade::Application.config.secret_token = '7765461351f0f7e0a9a4958b80300f71ec7e081318d322edb56a4c7d1a42f14c26f3c89a79941a69c84af11386ccf3089dfa6134f481d7eb661ba87b0d005f45'
